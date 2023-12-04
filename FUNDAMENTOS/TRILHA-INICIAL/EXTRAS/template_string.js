23e9e6b7bdc312174c5a3383a6380c0717c65274
MarckVinny <mvrpj@msn.com>
2023-12-03 23:28:40 -0300

    Extras

---
+++ FUNDAMENTOS/TRILHA-INICIAL/EXTRAS/template_string.js
@@ -0,0 +1,14 @@
+
+//! TEMPLATE STRING
+//* Para que funcione o Template, precisamos utilizar a
+//* crase `...` ao invés das aspas '...'
+//* E precisamos colocar o código javascript dentro de ${...}
+
+const a = 1
+const b = 2
+
+console.log(`a = ${a}, b = ${b}`)
+console.log(`a x b = ${a * b}`)
+console.log(`a x 2 = ${a * 2}`)
+console.log(`3 + 5 = ${3 + 5}`)
+console.log(`Número Aleatório gerado é: ${Math.random().toFixed(2)}`)
